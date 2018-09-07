e5384f6eb04ca693fca4030dc5290fee3b70e56e
JoyLubega <jollankent@gmail.com>
2018-09-07 20:59:56 +0300

    Add get request

--- api/models.py
+++ api/models.py
@@ -1,76 +1,77 @@
 import re
 from flask import request, abort, jsonify
 
-
-"""
-board: a nine-character string of spaces, x's and o's (lowercase).
-player: the character 'x' or 'o'
-"""
-
-def is_board_valid(board):
-    """ 
-    Check if both whether the board is a valid representation of a tictacoe board, and whether it could be o's turn. 
+class TicTacToe(object):
     """
-    valid = re.match(r'^[o x]{9}$', board) and (board.count("x") - board.count("o") in [0, 1])
-    if not valid:
-        abort('the board is not valid') 
-
-    return valid
-
-def opponent(player):
+    board: a nine-character string of spaces, x's and o's (lowercase).
+    player: the character 'x' or 'o'
     """
-    return the opponent
-    """
-    return jsonify({'o': 'x', 'x': 'o'}[player])
+    
+    def is_board_valid(self,board):
+        """ 
+        Check if both whether the board is a valid representation of a tictacoe board, and whether it could be o's turn. 
+        """
+        valid = re.match(r'^[o x]{9}$', board) and (board.count("x") - board.count("o") in [0, 1])
+        if valid == True:
+            return valid
+        else:
+            return False
+    
+    def opponent(self,player):
+        """
+        return the opponent
+        """
+        return {'o': 'x', 'x': 'o'}[player]
+    
+    def is_tie(self, board):
+        """ 
+        Checks if the result of the game is a tie
+        """
+        return not (" " in board)
+    
+    def is_winner(self,board, player):
+        """ 
+        Checks if the result of the game is a win
+        """
+        win = [player, player, player]
+        win_horizontal = [
+            [board[0], board[1], board[2]] == win,
+            [board[3], board[4], board[5]] == win,
+            [board[6], board[7], board[8]] == win
+        ]
+        win_vertical = [
+            [board[0], board[3], board[6]] == win,
+            [board[1], board[4], board[7]] == win,
+            [board[2], board[5], board[8]] == win
+        ]
+        win_diagonal = [
+            [board[0], board[4], board[8]] == win,
+            [board[2], board[4], board[6]] == win
+        ]
+        return any(win_horizontal) or any(win_vertical) or any(win_diagonal)
+    
+    def move(self, board, index, player):
+        """ 
+        Plays a move by player at that index, returning a new state of the board. 
+        """
+        splitted_board = list(board)
+        splitted_board[index] = player
+        return "".join(splitted_board)
+    
+    def expected_boards(self,board, player):
+        """ 
+        Returns a list of all boards that are expected from player's next move. 
+        """
+        return [self.move(board, i, player) for i, char in enumerate(board) if char == " "]
+    
+    def score(self,board, player):
+        """ Returns -1 if player will lose on this board, 0 if player can tie, and 1 if player can win. """
+        opp = self.opponent(player)
+        if self.is_winner(board, player):
+            return 1
+        if self.is_winner(board, opp):
+            return -1 
+        if self.is_tie(board):
+            return 0
 
-def is_tie(board):
-    """ 
-    Checks if the result of the game is a tie
-    """
-    return not (" " in board)
-
-def is_winner(board, player):
-    """ 
-    Checks if the result of the game is a win
-    """
-    win = [player, player, player]
-    win_horizontal = [
-        [board[0], board[1], board[2]] == win,
-        [board[3], board[4], board[5]] == win,
-        [board[6], board[7], board[8]] == win
-    ]
-    win_vertical = [
-        [board[0], board[3], board[6]] == win,
-        [board[1], board[4], board[7]] == win,
-        [board[2], board[5], board[8]] == win
-    ]
-    win_diagonal = [
-        [board[0], board[4], board[8]] == win,
-        [board[2], board[4], board[6]] == win
-    ]
-    return any(win_horizontal) or any(win_vertical) or any(win_diagonal)
-def move(board, index, player):
-    """ 
-    Plays a move by player at that index, returning a new state of the board. 
-    """
-    splitted_board = list(board)
-    splitted_board[index] = player
-    return "".join(splitted_board)
-
-def expected_boards(board, player):
-    """ 
-    Returns a list of all boards that are expected from player's next move. 
-    """
-    return [move(board, i, player) for i, char in enumerate(board) if char == " "]
-
-def score(board, player):
-    """ Returns -1 if player will lose on this board, 0 if player can tie, and 1 if player can win. """
-    opp = opponent(player)
-    if is_winner(board, player):
-        return 1
-    if is_winner(board, opp):
-        return -1 
-    if is_tie(board):
-        return 0
-
-    return max(-1 * score(candidate, opp) for candidate in expected_boards(board, player))
+        return max(-1 * self.score(candidate, opp) for candidate in self.expected_boards(board, player))
--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
-from flask import Flask, request, jsonify
-from views import *
+from flask import Flask, request, jsonify, abort
+from models import TicTacToe
 
 app = Flask(__name__)
 
+tic= TicTacToe()
 @app.route('/game')
 def newgame():
     board = request.args.get('board')
-    return board
+    if board is None:
+        return jsonify({'error':'enter the board please'})
+    if tic.is_board_valid(board)== False:
+        return jsonify({'error':'The board is invalid'})
+    possible_boards = tic.expected_boards(board, 'o') # all plays
+    winning_board=max(possible_boards, key=lambda candidate: -1 * tic.score(candidate, 'x'))
+    return jsonify({"next_board":winning_board})
+    
+    # return tic.expected_boards(board, player='o')[0]
+    
+    
+    # if tic.is_board_valid(board=board) == False:
+    #     return jsonify({'error':'The board is invalid'})
+
+    # or not tic.is_board_valid(board):
+    #    abort(400)
+    # return board
     
